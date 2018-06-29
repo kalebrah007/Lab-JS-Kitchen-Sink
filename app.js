e5fe07122852dc81961006824c9ea16492d0bc16
kalebrah007 <bayarea.collias@gmail.com>
2018-06-28 23:45:11 -0400

    submission

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var name = "Joe Collias";
+var person = "Joe Collias";
 const states = 50;
 var a = 5 + 4;
 console.log(a)
@@ -12,9 +12,9 @@ var sayHello = function () {
   alert('Hello World!')
 }
 sayHello();
-function checkAge(_name, _age){
-  if (_age < 21) {
-  alert('Sorry' + " " + _name + " " + 'you are not old enough to view this page!')
+function checkAge(name, age) {
+  if (age < 21) {
+    alert('Sorry' + " " + name + " " + 'you are not old enough to view this page!')
   }
 }
 checkAge("Charles", "21");
@@ -23,11 +23,53 @@ checkAge("James", "18");
 checkAge("John", "17");
 let favVeg = ['broccoli', 'artichoke', 'califlower'];
 var count = 0;
-while(count < favVeg.length){
+while (count < favVeg.length) {
   console.log(favVeg[count]);
   count++;
 }
+var arr = [{name: "Bill", age: 19,},
+  {name: "Ed", age: 20,},
+  {name: "Dixon", age: 21,},
+  {name: "Eve", age: 22,},
+  {name: "Erin", age: 23,}
+]
+var count = 0;
+while (count < arr.length) {
+  console.log(arr[count]);
+  count++
+  }
+checkAge("Bill", "19");
+checkAge("Ed", "20");
+checkAge("Dixon", "21");
+checkAge("Eve", "22")
+checkAge("Erin", "23");
+
+var getLength = "Hello World" 
+var b = 11; 
+  count = 0;
+for (var i = 0; i < getLength.length; i++){
+  if(getLength.charAt(i) === '11'){
+    count++;
+  }
+}
+console.log('The world is an odd place!')  
   
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+
+
+
+
 
 
 
