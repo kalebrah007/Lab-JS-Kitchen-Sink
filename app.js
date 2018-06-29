f883e7c2e8ae7bfef8782441b3751a006bbbc154
kalebrah007 <bayarea.collias@gmail.com>
2018-06-28 21:14:39 -0400

    updated lab thru ex.13

---
+++ app.js
@@ -0,0 +1,38 @@
+var name = "Joe Collias";
+const states = 50;
+var a = 5 + 4;
+console.log(a)
+var firstName = "Joe"
+if ("L".charCodeAt(0) >= firstName) {
+  alert('Back off the Line!');
+} else {
+  alert('Next!');
+} //alert is "Next!" because J is before L
+var sayHello = function () {
+  alert('Hello World!')
+}
+sayHello();
+function checkAge(_name, _age){
+  if (_age < 21) {
+  alert('Sorry' + " " + _name + " " + 'you are not old enough to view this page!')
+  }
+}
+checkAge("Charles", "21");
+checkAge("Abby", "27");
+checkAge("James", "18");
+checkAge("John", "17");
+let favVeg = ['broccoli', 'artichoke', 'califlower'];
+var count = 0;
+while(count < favVeg.length){
+  console.log(favVeg[count]);
+  count++;
+}
+  
+
+
+
+
+
+
+
+
